1348c25499ef66d3cc0c823bf7d6b7143a9fb637
TheSniperFan <9803861+TheSniperFan@users.noreply.github.com>
2019-05-29 23:20:52 +0200

    Add RepetitionSequence -Sequence that repeats its subtasks a number of times -Supports interleaved and blockwise repetition

---
+++ Fluid-HTN-Ext/Tasks/CompoundTasks/RepeatSequence.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FluidHTN.Compounds
+{
+    /// <summary>
+    ///     A sequence that repeats its subtasks a number of times.
+    /// </summary>
+    public class RepeatSequence : Sequence
+    {
+        /// <summary>
+        ///     Possible repetition types for RepeatSequences.
+        /// </summary>
+        public enum RepetitionType : byte
+        {
+            /// <summary>
+            ///     Entire sequence is repeated.
+            ///     A,B,C => A,B,C,A,B,C
+            /// </summary>
+            Interleaved,
+            /// <summary>
+            ///     Individual tasks are repeated.
+            ///     A,B.C => A,A,B,B,C,C
+            /// </summary>
+            Blockwise
+        }
+
+        protected readonly int Repetitions;
+        private readonly RepetitionType Type;
+
+        /// <summary>
+        ///     A sequence that repeats its subtasks a number of times.
+        /// </summary>
+        /// <param name="repetitions">How many repetitions to perform. (>=1)</param>
+        /// <param name="type">How to repeat the tasks.</param>
+        public RepeatSequence(int repetitions, RepetitionType type = RepetitionType.Interleaved)
+        {
+            if (repetitions < 1)
+            {
+                throw new ArgumentException("Cannot have fewer than 1 repetitions!", "repetitions");
+            }
+            else
+            {
+                Repetitions = repetitions;
+                Type = type;
+            }
+        }
+
+        protected override DecompositionStatus OnDecompose(IContext ctx, int startIndex, out Queue<ITask> result)
+        {
+            if (Type == RepetitionType.Interleaved)
+            {
+                PreDecomposeInterleaved();
+            }
+            else
+            {
+                PreDecomposeBlockwise();
+            }
+
+            return base.OnDecompose(ctx, startIndex, out result);
+        }
+
+        /// <summary>
+        ///     Duplicates every entry in the Subtasks list such that the entire
+        ///     sequence is performed the specified number of times.
+        /// </summary>
+        private void PreDecomposeInterleaved()
+        {
+            int num = Subtasks.Count;
+            for (int i = 0; i < Repetitions; ++i)
+            {
+                for (int j = 0; j < num; ++j)
+                {
+                    Subtasks.Add(Subtasks[j]);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Duplicates every entry in the Subtasks list such that every task
+        ///     is repeated the specified number of times before the agent moves
+        ///     on to the next task.
+        /// </summary>
+        private void PreDecomposeBlockwise()
+        {
+            int num = Subtasks.Count;
+            for (int i = num - 1; i >= 0; --i)
+            {
+                for (int j = 0; j < Repetitions - 1; ++j)
+                {
+                    Subtasks.Insert(i, Subtasks[i]);
+                }
+            }
+        }
+    }
+}
